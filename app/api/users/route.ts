9131f9d4a81da9e18fc7715db20b2f29f2c68959
dzidula <gamathodzidula@gmail.com>
2026-05-09 11:42:51 +0000

    feat: Creation de la route api/users pour recuperer tous les utilisateurs

---
+++ app/api/users/route.ts
@@ -0,0 +1,25 @@
+import connectDB from "@/libs/db";
+import User from '@/libs/models/utilisateurs';
+import { NextRequest, NextResponse } from "next/server"
+import mongoose from "mongoose";
+
+// GET /api/users → liste utilisateurs (admin)
+export const GET = async () => {
+        try {
+                await connectDB();
+                const users = await User.find({});
+                return new NextResponse(JSON.stringify(users), {
+                        status: 200,
+                        headers: { "Content-Type": "application/json" },
+                });
+        }
+        catch (error) {
+                console.error("Error in GET /api/users:", error);
+
+                return NextResponse.json({
+                        success: false,
+                        message: "Internal Server Error"
+                }, { status: 500 });
+
+        }
+}
